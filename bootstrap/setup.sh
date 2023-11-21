44a0f81786a38915fb22c5d741386083f00d033c
POP303U <nicoanton.gering@gmail.com>
2023-11-21 23:11:26 +0100

    Add: Bootstrapping script

---
+++ bootstrap/setup.sh
@@ -0,0 +1,6 @@
+cp -rv ../.config/alacritty ~/.config
+cp -rv ../.config/polybar  ~/.config
+cp -rv ../.config/kitty   ~/.config
+cp -rv ../.config/rofi   ~/.config
+cp -rv ../.config/i3    ~/.config
+# nice
