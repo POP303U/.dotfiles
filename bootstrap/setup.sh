dac9ffe3caeb48d4fbf49d2273137fdebdadeba8
POP303U <nicoanton.gering@gmail.com>
2023-11-23 20:23:30 +0100

    Update: Extract picom

--- bootstrap/setup.sh
+++ bootstrap/setup.sh
@@ -3,6 +3,7 @@ cp -rv ../.config/polybar  ~/.config
 cp -rv ../.config/kitty   ~/.config
 cp -rv ../.config/rofi   ~/.config
 cp -rv ../.config/i3    ~/.config
+cp -rv ../.config/picom ~/.config
 cp -rv ../.bashrc    ~
 cp -rv ../.tmux.conf ~
 # nice
