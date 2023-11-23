bdd63497a8e3adf45b5522dea9e810a2bf1d1087
POP303U <nicoanton.gering@gmail.com>
2023-11-23 20:22:37 +0100

    Add: Support for backing up dotfiles

---
+++ bootstrap/pull.sh
@@ -0,0 +1,9 @@
+cp -rv ~/.config/alacritty ../.config
+cp -rv ~/.config/picom ../.config
+cp -rv ~/.config/polybar  ../.config
+cp -rv ~/.config/kitty   ../.config
+cp -rv ~/.config/rofi   ../.config
+cp -rv ~/.config/i3    ../.config
+cp -rv ~/.bashrc    ..
+cp -rv ~/.tmux.conf ..
+# nice
