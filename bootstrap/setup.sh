a5a366e39ebe789d265a6c1c85696c604397ad46
POP303U <nicoanton.gering@gmail.com>
2023-11-25 22:35:04 +0100

    Update: more bootstrapping

--- bootstrap/setup.sh
+++ bootstrap/setup.sh
@@ -1,9 +1,12 @@
-cp -rv ../.config/alacritty ~/.config
-cp -rv ../.config/polybar  ~/.config
-cp -rv ../.config/kitty   ~/.config
-cp -rv ../.config/rofi   ~/.config
-cp -rv ../.config/i3    ~/.config
-cp -rv ../.config/picom ~/.config
-cp -rv ../.bashrc    ~
-cp -rv ../.tmux.conf ~
+cp -rv ../.config/alacritty     ~/.config
+cp -rv ../.config/i3blocks      ~/.config
+cp -rv ../.config/polybar       ~/.config
+cp -rv ../.config/kitty         ~/.config
+cp -rv ../.config/rofi          ~/.config
+cp -rv ../.config/i3            ~/.config
+cp -rv ../.config/picom         ~/.config
+cp -rv ../.bashrc               ~
+cp -rv ../.tmux.conf            ~
+
+chmod +x ~/.config/i3blocks/scripts *
 # nice
