6e5cc3500f32dc9382cb9e6d615c4851a4d40754
POP303U <nicoanton.gering@gmail.com>
2023-12-03 18:09:15 +0100

    Update: Add optional weather icon

--- .config/i3blocks/scripts/weather.pl
+++ .config/i3blocks/scripts/weather.pl
@@ -5,6 +5,8 @@ use warnings;
 
 my $zip_code = "85055";
 my $weather_info = `curl -s https://wttr.in/$zip_code?format="%t%20|%20%h%20|%20%w"`;
+# use weather icon if you like it
+# my $weather_icon = `curl -s https://wttr.in/$zip_code?format="%c"`;
 
 print "$weather_info";
 
