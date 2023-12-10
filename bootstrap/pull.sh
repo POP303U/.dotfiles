b48f538f60781fdab0ec23087dc44bc8880512c1
POP303U <nicoanton.gering@gmail.com>
2023-12-10 21:46:09 +0100

    Update: Pull/Push dunst also

--- bootstrap/pull.sh
+++ bootstrap/pull.sh
@@ -1,9 +1,11 @@
 rm -rf ../.config
 mkdir ../.config
+    
 cp -rv ~/.config/autoclicker    ../.config
 cp -rv ~/.config/alacritty      ../.config
 cp -rv ~/.config/i3blocks       ../.config
 cp -rv ~/.config/neofetch       ../.config
+cp -rv ~/.config/dunst          ../.config
 cp -rv ~/.config/polybar        ../.config
 cp -rv ~/.config/picom          ../.config
 cp -rv ~/.config/kitty          ../.config
